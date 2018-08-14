79bbbdf83dd1e2072e199cbd096db5a8c6e5e7e3
alexx617 <290477385@qq.com>
2018-08-14 18:20:02 +0800

    add

--- src/views/scroll/scroll.vue
+++ src/views/scroll/scroll.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="scroll-list">
-        
+        <ul>
+            <li v-for="(item,i) in lotter" :key="i" :ref="item" @click="showref">{{item}}</li>
+        </ul>
     </div>
 </template>
 
@@ -8,38 +10,46 @@
 const log = console.log;
 export default {
     props: ['cities'],
-    data () {
+    data() {
         return {
-            
         };
     },
     components: {},
     computed: {
-        
+        lotter() {
+            let lot = [];
+            for (let i in this.cities) {
+                lot.push(i)
+            }
+            return lot;
+        }
     },
     created() {
-        
     },
     methods: {
-        
+        showref(){
+            console.log(this.$refs);
+            
+        }
     },
 }
 
 </script>
 
 <style lang="scss" scoped>
-.scroll-list{
-    display: flex;
-    /* -webkit-box-pack: center; */
-    // -ms-flex-pack: center;
-    justify-content: center;
-    // -ms-flex-align: center;
-    align-items: center;
-    position: absolute;
-    top: 40px;
-    width: 20px;
-    right: 0;
-    bottom: 0;
-    background: #add8e6;
+.scroll-list {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: fixed;
+  width: 20px;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: #add8e6;
+  li {
+    text-align: center;
+    line-height: 1.5;
+  }
 }
 </style>
